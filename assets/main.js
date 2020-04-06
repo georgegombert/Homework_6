bae00c78f8e5a33227507d848acc08fa7c5dd10c
george-g <georgegombert@gmail.com>
2020-04-06 16:21:46 -0600

    Code refraction including class updatated names in css and JS notes

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,30 @@
 let citySearch ;
-let currentSearch;
 let apiHistory = JSON.parse(localStorage.getItem("searchHistory"));
-let key = "95ff62280315d2853674fe9ff4f63c2d";
+const key = "95ff62280315d2853674fe9ff4f63c2d";
 
-appendHistory();
+appendHistory(); // initalizing search history on page open
 
+// getting the search field value
 function search(){
     event.preventDefault();
     citySearch = $("#citySearch").val().trim();
     $("#citySearch").val("");
 }
 
+// add searched item to the history object and set max history count to 8
+function updateApiHistory(result){
+    if(apiHistory.length < 8){
+        apiHistory.push(result);
+        localStorage.setItem("searchHistory", JSON.stringify(apiHistory));
+    } 
+    else{
+        apiHistory = apiHistory.slice(1);
+        apiHistory.push(result);
+        localStorage.setItem("searchHistory", JSON.stringify(apiHistory));
+    }
+}
 
+// print the search history object to the screen
 function appendHistory(){
     if(!apiHistory){
         apiHistory = apiHistory = [];
@@ -23,33 +36,40 @@ function appendHistory(){
         historyDivNode.addClass("history-div");
         historyDivNode.text(apiHistory[city].name);
         $("#searchHistory").prepend(historyDivNode);
+        // if(apiHistory[city].name == historyDivNode.text()){
+        //     console.log("same");
+        // } else{
+        //     $("#searchHistory").prepend(historyDivNode);
+        // }
     }
 }
 
-function getCurrentWeather(){
+// main function that retrieves api information and prints weather to screen
+function getWeather(){
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/weather?q="+citySearch+"&appid="+key+"",
         method: "GET"
     })
     .then(function(result) {
+        //printing current weather
         $("#currentCity").text(""+result.name+" ("+moment().format("M/D/YY")+")");
         $("#currentIcon").attr({"src" : "https://openweathermap.org/img/wn/"+result.weather[0].icon+"@2x.png", "alt" : "Weather Icon"});
         $("#currentTemp").text(""+Math.round(((result.main.temp-273.15)*1.8)+32)+" ˙F");
         $("#currentHumidity").text(""+result.main.humidity+" %");
         $("#currentWind").text(""+result.wind.speed+" MPH");
         
+        // functions for printing future weather and appending the history
         updateApiHistory(result);
         appendHistory();
-        getCurrentUV(result.coord.lat, result.coord.lon);
-        getFutureWeather(result.id);
+        getUV(result.coord.lat, result.coord.lon); // uses latitude and longitude coordinates to get UV index
+        getFutureWeather(result.id); //uses city id to get forcast
     })
     .catch(function(error){
         alert("Please enter valid city");
     });
 }
 
-
-function getCurrentUV(latitude, longitude){
+function getUV(latitude, longitude){
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/uvi?appid="+key+"&lat="+latitude+"&lon="+longitude+"",
         method: "GET"
@@ -94,29 +114,17 @@ function getFutureWeather(cityId){
     });
 }
 
-function updateApiHistory(result){
-    if(apiHistory.length < 8){
-        apiHistory.push(result);
-        localStorage.setItem("searchHistory", JSON.stringify(apiHistory));
-    } 
-    else{
-        apiHistory = apiHistory.slice(1);
-        apiHistory.push(result);
-        localStorage.setItem("searchHistory", JSON.stringify(apiHistory));
-    }
-}
 
+// event listeners for search form and history buttons
 $("#searchButton").click(function(){
     search();
-    getCurrentWeather();
-    // appendHistory();
+    getWeather();
 });
 
 $("#citySearch").keydown(function(event){
     if(event.which == 13){
         search();
-        getCurrentWeather();
-        // appendHistory();
+        getWeather();
     }
 })
 
@@ -124,6 +132,7 @@ $("#searchHistory").click(function(event){
     let selection = event.target;
     if(selection.className == "history-div"){
         citySearch = selection.innerText;
-        getCurrentWeather();
+        getWeather();
     }
 })
+// end event listeners 
